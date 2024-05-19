66a5b9c76d23c18adde6aa26e5fc75b693254645
The Goose <ryanshaffer1324@gmail.com>
2024-05-19 16:59:42 -0400

    Added js code for dark mode button. Also styled the page layout a bit. Running into a bug when when the dark mod ebutton is clicked, the border color isn't changing how I want it to.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,25 @@
 // could probably do the light mode and dark mode here
 
 // First, one must set a variable connected to an html element with the following syntax - this is for the item to be clicked - in this instance, i chose a button with an id that will be consistent across my html files
-const lightModeDarkMode = document.querySelector(`#dark-mode-button`)
+const lightModeDarkMode = document.querySelector(`#dark-mode-button`);
 
 // Next, the entirety of the page needs to be changed after the click occurs
-const container = document.querySelector(`.container`)
+const container = document.querySelector(`.container`);
 
-// Next, one must setup a function that listens for a class and with the lass then implements a new style. It would seem i jsut setup the style in css?
-
+// Next, one must setup a function that listens for a click event - to set the default display of the page to light and then switch to dark, one must use the following syntax
+let mode = `dark`;
+lightModeDarkMode.addEventListener('click', function () {
+  // If mode is dark, apply light background
+  if (mode === 'dark') {
+    mode = 'light';
+    container.setAttribute('class', 'light');
+  }
+  // If mode is light, apply dark background
+  else {
+    mode = 'dark';
+    container.setAttribute('class', 'dark');
+  }
+});
 
 
 // now use the variable to add an event listener of click and run a function with the following parameters
