3cfc58e69d1a3e27868bf7bfb9b23338a88ad284
The Goose <ryanshaffer1324@gmail.com>
2024-05-20 18:40:45 -0400

    Attempting to implement specific styles with the light and dark mode. Running into a bug when switching between the mdoes in regards to border color. Trying to figure out why only certain borders change and are not the color theyre supposed to be given the respective view mode.

--- assets/javascript/form.js
+++ assets/javascript/form.js
@@ -2,6 +2,9 @@
 // Will also need to include a return of incomplete if the user doesnt fill out all the input boxes for the blog entry form
 const formSubmitButton = document.querySelector(`#form-submit-button`)
 const formDisplayMessage = document.querySelector(`#display-message`)
+const username = document.querySelector(`#username`)
+const title = document.querySelector(`#form-title`)
+const blogPost = document.querySelector(`#post-content`)
 
 function displayMessage(type, message) {
   formDisplayMessage.textContent = message;
@@ -11,24 +14,22 @@ function displayMessage(type, message) {
 formSubmitButton.addEventListener('click', function (event) {
   event.preventDefault();
 
-  const name = document.querySelector(`#name`).value
-  const title = document.querySelector(`#form-title`).value
-  const blogPost = document.querySelector(`#post-content`).value
-
-  if (name === '') {
+  if (username === '') {
     displayMessage('error', 'Name cannot be blank');
   } else if (title === '') {
     displayMessage('error', 'Title cannot be blank');
   } else if (blogPost === '') {
     displayMessage('error', 'Blog post cannot be blank');
   } else {
-    displayMessage(`success`, `Your post has been saved`)
+    displayMessage(`success`, `Your post has been saved`);
   }
 
+  const blogContent = {
+    name: username.value,
+    title: title.value,
+    blogPost: blogPost.value
 
+  };
 
-    // TODO: Save email and password to localStorage and render the last registered user
-    localStorage.setItem(`name`, name);
-    localStorage.setItem(`title`, title);
-    localStorage.setItem(`blog-post`, blogPost);
-}) 
+ localStorage.setItem(`blogContent`, JSON.stringify(blogContent));
+}); 
