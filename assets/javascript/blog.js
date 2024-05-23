d95d715331e2da5298bb2a3879e33a1fac506b88
The Goose <ryanshaffer1324@gmail.com>
2024-05-22 20:05:20 -0400

    Attempted working out the functuon for appending blog posts to the blog post .html page. Ran into time constraints and bugs. Will work later.

--- assets/javascript/blog.js
+++ assets/javascript/blog.js
@@ -1,2 +1,30 @@
 // This page will need a header that can take one back to the form entry page but when doing so stores the display selection thus carrying it over to the previous page
 // I think i will need to structure blank cards on the blot html file to append stuff to
+const bodyEl = document.body
+function init(){
+  const blogContentArray = JSON.parse(localStorage.getItem('blogContentArray')) || []
+// define function
+for (let index = blogContentArray.length; index >= 0; index--) {
+  const cardEl = document.createElement('card')
+  const titleEl = document.createElement('h1')
+  const contentEl = document.createElement('p')
+  const authorEl = document.createElement('p')
+
+  authorEl.textContent = blogContentArray[0]
+  titleEl.textContent = blogContentArray[1]
+  contentEl.textContent = blogContentArray[2]
+
+  cardEl.appendChild(authorEl)
+  cardEl.appendChild(titleEl)
+  cardEl.appendChild(contentEl)
+
+  cardEl.setAttribute('class', 'blog-post-card')
+
+
+  
+}
+}
+// then i need a for loop
+
+// then call the fucntion
+// first create element, then text, then append
